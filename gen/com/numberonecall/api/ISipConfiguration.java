c7c3cf09c8f8539cf7ade58c991a7224c8aa7256
Michał Łaszczewski <michal@emikse.com>
2018-02-26 12:06:39 +0100

    webview sign up removed bin directory

--- gen/com/numberonecall/api/ISipConfiguration.java
+++ gen/com/numberonecall/api/ISipConfiguration.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: E:\\Sunil Prpjects\\sip1\\prifix\\FLORIN NUBER ONE\\CSipSimple\\src\\com\\numberonecall\\api\\ISipConfiguration.aidl
+ * Original file: /Users/m8/pajeet/android-numberone-adore-dialer/src/com/numberonecall/api/ISipConfiguration.aidl
  */
 package com.numberonecall.api;
 public interface ISipConfiguration extends android.os.IInterface
